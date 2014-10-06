8bd1652504819e4b555e20a3d9af137b9ee641e4
atthegate <jon.engelsman@gmail.com>
2014-10-05 17:40:05 -0700

    A Leaflet/D3 demo for drones.
    This simple Leaflet map allows you to randomly place a specified number
    of waypoint drones on the map. You can then drag-and-drop a (draggable)
    mission drone on to the map, which will display D3 lines of varying
    color between the mission and way point drones. This lines are a dynamic
    indication of a somewhat arbitrary prioritization of distances between
    mission and waypoint drones.

---
+++ js/tool/map.js
@@ -0,0 +1,207 @@
+// Map controller object
+function object_map(){	
+	
+	////// Parameters //////
+	
+	// Leafet layers
+	this.layers = {
+		hq: [],
+		drone_waypoint: [],
+		drone_mission: []
+	}
+	
+	// Leaflet markers
+	this.markers = {
+		hq: [],
+		drone_mission: [],
+		drone_waypoint: []
+	};
+	
+	// Specify icon types (L.Icon extensions)
+	var icon_types = {
+		hq: L.Icon.extend({options: {iconSize: [50, 50], iconAnchor: [25, 25],	popupAnchor: [0, -15]}}),
+		drone: L.Icon.extend({options: {iconSize: [50, 50], iconAnchor: [25, 25],	popupAnchor: [0, -15]}})
+	};
+	
+	// Specify icon images
+	var icons = {
+		skyward: new icon_types.hq({iconUrl: 'icons/icon-skyward.png'}),
+		drone_mission: new icon_types.drone({iconUrl: 'icons/icon-drone_mission.png'}),
+		drone_waypoint: new icon_types.drone({iconUrl: 'icons/icon-drone_waypoint.png'})
+	}
+
+	////// Methods //////
+	
+	// Initialize map
+	this.init_map = function(lat,lng,zoom){
+	
+		// Leaflet map
+		v_map.map = L.map('map', {center: [lat, lng], zoom: zoom, worldCopyJump: true});		
+				
+		// Create base layers and overlays
+		// ESRI maps w/ ASTER Terrain overlays
+		L.control.layers({
+			'Topographic Map': L.esri.basemapLayer("Topographic",{continuousWorld: false, noWrap: true}).addTo(v_map.map),
+			'Imagery Map': L.esri.basemapLayer("Imagery",{continuousWorld: false, noWrap: true}),
+			'Gray Map': L.esri.basemapLayer("Gray",{continuousWorld: false, noWrap: true}),
+			},
+			{'ASTER GDEM & SRTM Hillshade': L.tileLayer(
+				'http://129.206.74.245:8004/tms_hs.ashx?x={x}&y={y}&z={z}',
+				{continuousWorld: false, noWrap: true}
+			),
+			'ASTER GDEM Contour': L.tileLayer(
+				'http://129.206.74.245:8006/tms_il.ashx?x={x}&y={y}&z={z} ',
+				{continuousWorld: false, noWrap: true}
+			)
+		}).addTo(v_map.map);
+		
+		// Mouse position display
+		L.control.mousePosition().addTo(v_map.map);
+		
+		// *** Add SkyWard IO HQ *** 
+		
+		// Create popup text
+		var pu = '<img src="images/img_hq.png"></><br><b>SkyWard IO, Inc.</b><br>233 SW Naito Pkwy.<br>Portland, OR 97204';
+		
+		// Create HQ marker
+		v_map.markers.hq = [L.marker([lat, lng], {icon: icons.skyward}).bindPopup(pu)];
+		
+		// Add HQ marker to layer and add to map
+		v_map.layers.hq = L.layerGroup(v_map.markers.hq).addTo(v_map.map);
+	
+		// Define draggable start/stop functions for new mission drone element 
+		$(".drag").draggable({
+		  helper: 'clone',
+		  stack: ".drag",
+		  containment: 'map',
+		  zIndex: 10000,
+		  appendTo: 'body',
+		  start: function(evt, ui) {
+			  $('#drone_mission').fadeTo('fast', 0.6, function() {});
+			  v_map.clearMissionDrone(); // Clear mission Drone from map
+		  },
+		  stop: function(evt, ui) {
+			  $('#drone_mission').fadeTo('fast', 1.0, function() {});
+				var offset = $('#map').offset(); // Get offset of 'map' element
+				var size = v_map.map.getSize(); // Get Leaflet size of map
+				// Check to make sure that the drag end position is in map
+				if((ui.offset.left >= offset.left  & ui.offset.left <= (offset.left + size.x)) & 
+				(ui.offset.top >= offset.top & ui.offset.top <= (offset.top + size.y))){
+					v_map.addMissionDrone(evt); // Add new mission drone to map
+					v_tool.updateMissionDrone(); // Update mission drone information				
+				}
+		  }
+		});
+		
+		
+		// Set map viewreset function 
+		v_map.map.on("viewreset", v_map.resetView);
+				
+		// Create SVG layer for D3 overlay
+		v_map.map._initPathRoot(); // Initialize the SVG layer 
+		v_map.svg_layer = {};
+		v_map.svg_layer.svg = d3.select("#map").select("svg");
+		v_map.svg_layer.g = v_map.svg_layer.svg.append("g");
+		
+	}
+		
+	// Set base map layer
+	this.setBaseMap = function(maptype){
+		v_map.pcv_map.removeLayer(v_map.pcv_map.current_layer);
+		v_map.pcv_map.current_layer = L.esri.basemapLayer(maptype);
+		v_map.pcv_map.addLayer(v_map.pcv_map.current_layer);
+	}
+	
+	// Reset view function for Leaflet
+	this.resetView = function(){
+		if(v_map.markers.drone_mission.length > 0 && v_map.markers.drone_waypoint.length > 0){
+			v_tool.plotLines(); // Redraw mission-bad drone lines on zoom
+		}
+	}
+	
+	// Clear mission drone marker from map
+	this.clearMissionDrone = function(){
+		v_map.map.removeLayer(v_map.layers.drone_mission);
+		v_map.markers.drone_mission = [];
+		
+	}
+	
+	// Add mission drone to map
+	this.addMissionDrone = function(evt){
+		
+		// Inputs
+		// evt = mission drone icon draggable stop event
+
+		// Get LatLng position of dragend point (mission drone location)
+		var latlng = v_map.map.containerPointToLatLng(v_map.map.mouseEventToContainerPoint(evt));
+	
+		// Create popup for mission drone
+		var pu = '<b>mission Drone</b><br>Lat: ' + latlng.lat.toFixed(4) + '<br>Lng: ' + latlng.lng.toFixed(4);
+		
+		// Create mission drone marker
+		v_map.markers.drone_mission = [
+			L.marker(latlng, {icon: icons.drone_mission, draggable: true}).bindPopup(pu)
+			.on('dragstart', function(e){
+				v_tool.clearLines(); // Clear mission-bad drone lines on dragstart
+			})
+			.on('dragend', function(e){
+				latlng = e.target.getLatLng(); // Get LatLng of dragend position
+				// Create popup
+				var pu = '<b>mission Drone</b><br>Lat: ' + latlng.lat.toFixed(4) + '<br>Lng: ' + latlng.lng.toFixed(4);
+				this.bindPopup(pu); // Bind popup to new mission drone marker
+				v_tool.updateMissionDrone(); // Update mission drone information
+			})
+		];
+		
+		// Add mission drone layer to map
+		v_map.layers.drone_mission = L.layerGroup(v_map.markers.drone_mission).addTo(v_map.map);
+		
+	}
+	
+	// Clear all bad drones from map
+	this.clearWaypointDrones = function(){
+	
+		v_map.map.removeLayer(v_map.layers.drone_waypoint);
+		v_map.markers.drone_waypoint = [];
+		
+	}
+		
+	// Generate new bad drones randomly on map
+	this.generateWaypointDrones = function(bounds, num_drone_waypoint){
+		
+		// Inputs	
+		// bounds = SW/NE bounds of Leaflet map (current zoom)
+		// num_drone_waypoint = input number of bad drones
+
+		this.clearWaypointDrones(); // Clear all bad drones
+		
+		// Loop through number of bad drones
+		for (var ii = 0; ii < num_drone_waypoint; ii++){
+		
+			// Calculate lat/lng range of Leaflet zoom
+			var d_lat = Math.abs(bounds._northEast.lat - bounds._southWest.lat);
+			var d_lng = Math.abs(bounds._northEast.lng - bounds._southWest.lng);
+			
+			// Generate random lat/lng within range
+			var lat = Math.random() * d_lat + bounds._southWest.lat;
+			var lng = Math.random() * d_lng + bounds._southWest.lng;
+			
+			// Create popup for each bad drone
+			var pu = '<b>Waypoint Drone #' + (ii+1) + '</b><br>Lat: ' + lat.toFixed(4) + '<br>Lng: ' + lng.toFixed(4);
+			
+			// Crate bad drone marker
+			v_map.markers.drone_waypoint.push(L.marker([lat, lng], {icon: icons.drone_waypoint}).bindPopup(pu));
+		}
+		
+		// Add waypoint drone marker set to layer and map
+		v_map.layers.drone_waypoint = L.layerGroup(v_map.markers.drone_waypoint).addTo(v_map.map);
+	
+	}
+	
+	// Convert Leaflet LatLng to pixel points
+	this.projectLatLng = function(x){
+		point = v_map.map.latLngToLayerPoint(x);
+		return [point.x, point.y];
+	}
+		
+}
---
+++ js/tool/tool.js
@@ -0,0 +1,163 @@
+// Tool controller object
+function object_tool(){
+		
+	// Define GeographicLib.Geodesic function!
+	this.geod = GeographicLib.Geodesic.WGS84;
+	
+	// Initialize Tool object
+	this.init_tool = function(){	
+
+		var that = this; 
+		
+		// Initialize map to Skyward IO HQ
+		v_map.init_map(45.520369, -122.671264, 16);
+		
+		// Define click function for add random waypoint drones button
+		$('#btn_rdm_drone_waypoint').click(function(){
+			
+			// Get input number of waypoint drones
+			var num_drone_waypoint_tmp = $('#input_drone_waypoint').val();
+
+			// Check that input number of waypoint drones is real number
+			if(that.isNumber(num_drone_waypoint_tmp)){
+				var num_drone_waypoint = num_drone_waypoint_tmp;
+			}else{
+				// If not a number, default to 1
+				var num_drone_waypoint = 1;
+				$('#input_drone_waypoint').val(num_drone_waypoint);
+			}
+			
+			// Get LatLng bounds of Leaflet current zoom
+			var bounds = v_map.map.getBounds();
+			
+			// Generate waypoint drones
+			v_map.generateWaypointDrones(bounds, num_drone_waypoint);
+
+			// Update mission drone information
+			v_tool.updateMissionDrone();
+			
+		})
+		
+		// Define click function for clear all drones button
+		$('#btn_clear_drone_all').click(function(){
+			v_map.clearWaypointDrones(); // Clear all waypoint drones
+			v_map.clearMissionDrone(); // Clear mission drone
+			that.clearLines();
+		})
+	
+	}
+	
+	// Update mission drone information
+	this.updateMissionDrone = function(){
+		// If mission drone exists...
+		if(v_map.markers.drone_mission.length > 0){
+			this.plotLines(); // Plot mission-waypoint drone lines
+		}
+	}
+	
+	// Clear all mission-waypoint drone lines
+	this.clearLines = function(){
+		d3.selectAll("line").remove()
+	}	
+	
+	// Plot mission-waypoint drone lines
+	this.plotLines = function(){
+
+		var that = this;
+		
+		this.clearLines(); // Clear all mission-waypoint drone lines
+		
+		// Get pixel xy points of mission drone LatLng
+		var point_drone_mission = v_map.projectLatLng(v_map.markers.drone_mission[0]._latlng);
+		
+		// Calculate distance/bearing between mission drone and HQ
+		// Assume Leaflet coordinate system is WGS84!
+		var geo_mission2hq = this.geod.Inverse(
+			v_map.markers.hq[0]._latlng.lat, 
+			v_map.markers.hq[0]._latlng.lng, 
+			v_map.markers.drone_mission[0]._latlng.lat, 
+			v_map.markers.drone_mission[0]._latlng.lng
+		);
+		
+		// Initialize relative distance arrays
+		var rel_distances_waypoint2hq = [];
+		var rel_distances_mission2hq = [];
+		var rel_distances_mission2waypoint = [];
+		
+		// Loop through each waypoint drone...
+		$.each(v_map.markers.drone_waypoint, function(idx, drone_waypoint){
+		
+			// Calculate distance/bearing between mission and waypoint drones
+			var ge0_mission2waypoint = that.geod.Inverse(
+				v_map.markers.drone_mission[0]._latlng.lat, 
+				v_map.markers.drone_mission[0]._latlng.lng, 
+				drone_waypoint._latlng.lat, 
+				drone_waypoint._latlng.lng
+			);
+			
+			// Calculate distance/bearing between waypoint drone and HQ
+			var geo_waypoint2hq = that.geod.Inverse(
+				v_map.markers.hq[0]._latlng.lat, 
+				v_map.markers.hq[0]._latlng.lng, 
+				drone_waypoint._latlng.lat, 
+				drone_waypoint._latlng.lng
+			);
+			
+			// Define relative distance metric and add to array
+			// Arbirtary strength of distance between 3 interactions:
+			// - mission drone & HQ
+			// - mission drone & waypoint drone
+			// - waypoint drone & HQ 
+			
+			// Arbitrary metric
+			var metric = ge0_mission2waypoint.s12 * geo_waypoint2hq.s12 / geo_mission2hq.s12;
+			
+			// Add metric value to relative distance array
+			rel_distances_mission2waypoint.push(metric);
+		
+			// Define default HSV color of mission-waypoint drone line
+			var hue = 120, sat = 100, val = 50;
+			var color = 'hsl(' + hue + ',' + sat + '%,' + val + '%)';
+			
+			// Get pixel xy points of waypoint drone LatLng
+			var point_drone_waypoint = v_map.projectLatLng(drone_waypoint._latlng);
+		
+			// Draw D3 line between mission and waypoint drone
+			var myLine = v_map.svg_layer.svg.append("svg:line")
+				.attr("x1", point_drone_waypoint[0])
+				.attr("y1", point_drone_waypoint[1])
+				.attr("x2", point_drone_mission[0])
+				.attr("y2", point_drone_mission[1])
+				.style("stroke", color)
+				.style("stroke-width", 5);
+		})
+		
+		// Select all mission-waypoint drone lines and apply new stroke color
+		d3.selectAll('line').style("stroke", function(d,ii){
+			
+			// Get relative distance metric
+			// NOTE: Assumes array order matches between D3 lines and markers.
+			var rel_distance = rel_distances_mission2waypoint[ii];
+
+			// Calculate max/min relative distance
+			var max_rel_distance = Math.max.apply(Math, rel_distances_mission2waypoint);
+			var min_rel_distance = Math.min.apply(Math, rel_distances_mission2waypoint);
+			
+			// Define HSV color range (red=0 to green=120)
+			var hue_min = 0, hue_max = 120;
+			var sat = 100, val = 50;
+			
+			// Calculate hue based on relative distance
+			var hue = (((rel_distance - min_rel_distance) * (hue_max - hue_min)) / (max_rel_distance - min_rel_distance)) + hue_min;
+			var color = 'hsl(' + hue + ',' + sat + '%,' + val + '%)';
+			return color;
+		})
+
+	}
+	
+	// Real number check
+	this.isNumber = function(n){
+		return !isNaN(parseFloat(n)) && isFinite(n);
+	}
+	
+}
